a03f5dd275fe0a3b464914393ffa2b41a9630a25
pharbst <pharbst@student.42heilbronn.de>
2022-09-25 07:28:33 +0200

    first raw scripting sed command not working but input reading working poperly

---
+++ change_ssh_port.sh
@@ -0,0 +1,24 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    change_ssh_port.sh                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/09/25 07:08:31 by pharbst           #+#    #+#              #
+#    Updated: 2022/09/25 07:26:08 by pharbst          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+!#/bin/bash
+
+read -p "Wanna change the ssh port?" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+read -p "which port u wanna use for ssh?" sshport
+echo "you wanna change ssh port to $sshport and also open the port in the firewall UFW if u use another firewall change the script if u dont use a firewall u can continue"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+sudo sed 's/.*Port .*/Port $sshport/' /etc/ssh/sshd_config
+sudo ufw allow $sshport
+echo "if the followed output is correct eveything worked properly"
+grep -F "Port " /etc/ssh/sshd_config
+sudo ufw status numbered | grep "$sshport"
+echo Done
